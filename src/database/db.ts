3da233d0ab73f8fe6295e67b09f07225bbc746e4
razakwasiul <razakwasiu@gmail.com>
2022-01-19 10:18:38 +0000

    stable

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,6 +2,6 @@ import knex from 'knex';
 import configs from './knexfile';
 
 const environment = process.env.ENVIRONMENT || 'development';
-const config = configs[environment];
+const config = configs['development'];
 
 export const database = knex(config);
