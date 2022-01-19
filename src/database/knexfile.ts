c29c49cb918723f6079870b545f372de6d3c44d3
razakwasiul <razakwasiu@gmail.com>
2022-01-19 06:41:28 +0000

    heroku build fix

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -5,20 +5,21 @@ import { env } from '../helpers/env-helper';
 interface IKnexConfig {
   [key: string]: Knex.Config;
 }
+const connectSetting = process.env.DATABASE_URL || {
+  host: env.string('DB_DEV_HOST', 'ec2-3-225-41-234.compute-1.amazonaws.com'),
+  port: env.number('DB_DEV_PORT', 5432),
+  database: env.string('DB_DEV_DATABASE', 'd97ppci557sp5o'),
+  user: env.string('DB_DEV_USER', 'nokjjuzwdavqlx'),
+  password: env.string('DB_DEV_PASS', '03414f761fa3b9567388a5554807cbcc3fa23edf90ece4f69b364cb92a3000a4'),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 const configs: IKnexConfig = {
   development: {
     client: 'postgresql',
-    connection: {
-      host: env.string('DB_DEV_HOST', 'ec2-3-225-41-234.compute-1.amazonaws.com'),
-      port: env.number('DB_DEV_PORT', 5432),
-      database: env.string('DB_DEV_DATABASE', 'd97ppci557sp5o'),
-      user: env.string('DB_DEV_USER', 'nokjjuzwdavqlx'),
-      password: env.string('DB_DEV_PASS', '03414f761fa3b9567388a5554807cbcc3fa23edf90ece4f69b364cb92a3000a4'),
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    connection: connectSetting,
     pool: {
       min: 2,
       max: 10,
@@ -56,16 +57,7 @@ const configs: IKnexConfig = {
   },
   production: {
     client: 'postgresql',
-    connection: {
-      host: env.string('DB_DEV_HOST', 'ec2-3-225-41-234.compute-1.amazonaws.com'),
-      port: env.number('DB_DEV_PORT', 5432),
-      database: env.string('DB_DEV_DATABASE', 'd97ppci557sp5o'),
-      user: env.string('DB_DEV_USER', 'nokjjuzwdavqlx'),
-      password: env.string('DB_DEV_PASS', '03414f761fa3b9567388a5554807cbcc3fa23edf90ece4f69b364cb92a3000a4'),
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    connection: connectSetting,
     pool: {
       min: 2,
       max: 10,
@@ -74,6 +66,17 @@ const configs: IKnexConfig = {
       tableName: 'knex_migrations',
     },
   },
+  // production: {
+  //   client: 'postgresql',
+  //   connection:
+  //       pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations',
+  //   },
+  // },
 };
 
 /*
