6f688365a3b2808b063e8c2686f029463e609b2b
razakwasiul <razakwasiu@gmail.com>
2022-01-19 06:08:38 +0000

    heroku build fix

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import { ioMiddleware } from './middlewares/ioMiddleware';
 import ConnectionHandler from './socket/Connection.Handler';
 import chatHandler from './socket/Chat.Handler';
 import BlockHandler from './socket/Block.Handler';
+import { getAuthenticatedUserDetails } from './middlewares/get-authenticated-user-info copy';
 
 type SocketNextFunc = (err?: ExtendedError | undefined) => void;
 
@@ -32,6 +33,7 @@ const server = http
   .listen(PORT)
   .once('listening', () => logger.info(`🚀 Server is up and running at http://${HOST}:${PORT}`));
 
+const adaptSocketToExpressMiddleWares = (middleware: Function) => (socket: Socket, next: SocketNextFunc) => middlew;
 ConnectIo(server);
 function ConnectIo(server: any) {
   const io = new Server(server, {
@@ -41,7 +43,9 @@ function ConnectIo(server: any) {
     },
   });
 
-  io.use(ioMiddleware);
+  io.use(adaptSocketToExpressMiddleWares(getAuthenticatedUserDetails));
+
+  // io.use(ioMiddleware);
 
   io.on('connection', async function (socket) {
     await ConnectionHandler(io, socket);
