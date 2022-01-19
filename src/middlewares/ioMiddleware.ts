b71e04660ca57fcf5be8e1b51343142a447000b4
razakwasiul <razakwasiu@gmail.com>
2022-01-19 08:30:32 +0000

    stable

--- src/middlewares/ioMiddleware.ts
+++ src/middlewares/ioMiddleware.ts
@@ -6,12 +6,10 @@ import { logger } from '../helpers/logger';
 
 type SocketNextFunc = (err?: ExtendedError | undefined) => void;
 
-const AUTH0_DOMAIN = env.string('AUTH0_DOMAIN', 'localhost');
-
 export const ioMiddleware = async (socket: Socket, next: SocketNextFunc) => {
   const bearerToken = socket.request.headers.authorization;
   // try {
-  const response = await axios.get(`${AUTH0_DOMAIN}/userinfo`, {
+  const response = await axios.get(`https://dev-tgdlv40g.us.auth0.com/userinfo`, {
     // @ts-ignore
     headers: { Authorization: bearerToken },
   });
